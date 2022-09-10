6bbd2b2945b7a8b4688e64f6c0ef3eb211f1a0fc
Vishal Yadav <vishalyadav1746@gmail.com>
2022-09-10 18:40:46 +0530

    fix: check if package found via apt-search.

--- linux-script.sh
+++ linux-script.sh
@@ -10,19 +10,25 @@
 # Packages to install
 # This is a list of packages to install.
 
-packages=(git gitk vim sublime-text brave chromium chrome intellij-toolbox code Postman mongodb mysql)
+packages=(git gitk vim sublime-text brave chromium chrome intellij-toolbox code Postman mongodb)
 echo "Installing packages..."
 
 # The script will install these packages.
 # The script will also install the packages that are required by the packages in this list.
 
-for name in ${packages[@]}; do
+for name in "${packages[@]}"; do
     echo "Installing $name..."
-    if [ ! -x "$(command -v $name)" ]; then
+    if [ ! -x "$(command -v "$name")" ]; then
         echo "Package $name is not installed. Do you want to install it? (y/n)"
-        read answer
+        read -r answer
         if [ "$answer" == "y" ]; then
-            sudo apt-get install $name
+            search_result=$(sudo apt-cache search --names-only "$name" || wc -l)
+            if [ "$search_result" -gt 0 ]; then
+                sudo apt-get install "$name"
+            fi
+            else
+                # TODO: tar file or download via source
+                echo "Package not found via apt-search."
         fi
         else
             echo "Package $name is already installed."
